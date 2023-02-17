36a7f22ba937b894be7f20d1d6244766d29f041e
lloydzhou <lloydzhou@qq.com>
2023-02-17 18:55:03 +0800

    refactor: using x6-hook for mindmap demo

--- examples/x6-example-features/src/pages/case/mind.tsx
+++ examples/x6-example-features/src/pages/case/mind.tsx
@@ -4,6 +4,10 @@ import { connectors } from '../connector/xmind-definitions'
 import Hierarchy from '@antv/hierarchy'
 import { Selection } from '@antv/x6-plugin-selection'
 import { Keyboard } from '@antv/x6-plugin-keyboard'
+
+import { useGraphState } from 'x6-hooks/react'
+import { useEffect, useCallback, useState, useMemo, useRef } from 'react'
+
 import '../index.less'
 import './mind.less'
 
@@ -172,14 +176,160 @@ const data: MindMapData = {
   ],
 }
 
-export default class Example extends React.Component {
-  private container: HTMLDivElement
+export const useMindmapState = (initData = data) => {
+  const { nodes, edges, setNodes, setEdges, graph, setGraph } = useGraphState()
+  const [state, setState] = useState(initData)
+  const result = useMemo(() => {
+    return Hierarchy.mindmap(state, {
+      direction: 'H',
+      getHeight(d) {
+        return d.height
+      },
+      getWidth(d) {
+        return d.width
+      },
+      getHGap() {
+        return 40
+      },
+      getVGap() {
+        return 20
+      },
+      getSide: () => {
+        return 'right'
+      },
+    })
+  }, [state])
+  const idMap = useMemo(() => {
+    const index = (idMap, item) => {
+      idMap[item.id] = item
+      if (item.children) {
+        return item.children.reduce(index, idMap)
+      }
+      return idMap
+    }
+    return [state].reduce(index, {})
+  }, [state])
 
-  componentDidMount() {
+  useEffect(() => {
+    console.log('result', result)
+    const traverse = (res, item) => {
+      res.nodes.push({
+        ...item.data,
+        shape: item.data.type === 'topic-child' ? 'topic-child' : 'topic',
+        x: item.x,
+        y: item.y,
+        children: undefined,
+      })
+      if (res.parent) {
+        res.edges.push({
+          id: `${res.parent}:${item.data.id}`,
+          shape: 'mindmap-edge',
+          source: {
+            cell: res.parent,
+            anchor:
+              item.data.type === 'topic-child'
+                ? {
+                    name: 'right',
+                    args: {
+                      dx: -16,
+                    },
+                  }
+                : {
+                    name: 'center',
+                    args: {
+                      dx: '25%',
+                    },
+                  },
+          },
+          target: {
+            cell: item.data.id,
+            anchor: {
+              name: 'left',
+            },
+          },
+        })
+      }
+      if (item.children) {
+        const parent = res.parent
+        res.parent = item.data.id
+        const r = item.children.reduce(traverse, res)
+        res.parent = parent
+        return r
+      }
+      return res
+    }
+    const res = [result].reduce(traverse, { nodes: [], edges: [], parent: '' })
+    console.log('result', res)
+    setNodes(res.nodes)
+    setEdges(res.edges)
+    setTimeout(() => {
+      if (graph.current) {
+        graph.current.centerContent()
+      }
+    }, 100)
+  }, [result])
+
+  useEffect(() => {
+    console.log('idMap', idMap, nodes, edges)
+  })
+
+  const addTopic = useCallback(
+    ({ node }) => {
+      console.log('addTopic', node)
+      const { id, type } = node.getProp()
+      const item = idMap[id]
+      if (item) {
+        const children = item.children || []
+        item.children = children.concat({
+          id: `${id}-${children.length + 1}`,
+          type: type === 'topic' ? 'topic-branch' : 'topic-child',
+          label: `${type === 'topic' ? '分支主题' : '子主题'}${
+            children.length + 1
+          }`,
+          width: type === 'topic' ? 100 : 60,
+          height: type === 'topic' ? 40 : 30,
+        })
+        setState({ ...state }) //
+      }
+    },
+    [state],
+  )
+
+  useEffect(() => {
+    console.log('add event')
+    if (graph.current) {
+      graph.current.on('add:topic', addTopic)
+    }
+    return () => {
+      if (graph.current) {
+        graph.current.off('add:topic', addTopic)
+      }
+    }
+  }, [graph.current])
+  return {
+    nodes,
+    edges,
+    setNodes,
+    setEdges,
+    graph,
+    setGraph,
+  }
+}
+
+export default function App() {
+  const { nodes, setNodes, edges, setEdges, graph, setGraph } =
+    useMindmapState()
+
+  const container = useRef()
+  useEffect(() => {
     const graph = new Graph({
-      container: this.container,
+      container: container.current,
       width: 800,
       height: 600,
+      grid: true,
+      interacting: {
+        nodeMovable: false,
+      },
       connecting: {
         connectionPoint: 'anchor',
       },
@@ -192,214 +342,11 @@ export default class Example extends React.Component {
       enabled: true,
     })
     graph.use(keyboard)
-
-    const render = () => {
-      const result: HierarchyResult = Hierarchy.mindmap(data, {
-        direction: 'H',
-        getHeight(d: MindMapData) {
-          return d.height
-        },
-        getWidth(d: MindMapData) {
-          return d.width
-        },
-        getHGap() {
-          return 40
-        },
-        getVGap() {
-          return 20
-        },
-        getSide: () => {
-          return 'right'
-        },
-      })
-      const traverse = (hierarchyItem: HierarchyResult) => {
-        if (hierarchyItem) {
-          const { data, children } = hierarchyItem
-          // 检查当前遍历的节点已经存在还是需要新添加？
-          if (graph.hasCell(data.id)) {
-            const node = graph.getCellById(data.id)
-            node.prop('position', { x: hierarchyItem.x, y: hierarchyItem.y })
-          } else {
-            const node = graph.addNode({
-              id: data.id,
-              shape: data.type === 'topic-child' ? 'topic-child' : 'topic',
-              x: hierarchyItem.x,
-              y: hierarchyItem.y,
-              width: data.width,
-              height: data.height,
-              label: data.label,
-              type: data.type,
-            })
-          }
-          if (children) {
-            children.forEach((item: HierarchyResult) => {
-              const { id, data } = item
-              // 先遍历子节点（里面包含创建逻辑，如果画布没有开启async的时候，创建边会提示找不到target节点）
-              traverse(item)
-              const eid = `${hierarchyItem.id}-->${id}`
-              // 检查当前边是否已经存在
-              if (!graph.hasCell(eid)) {
-                graph.addEdge({
-                  id: eid,
-                  shape: 'mindmap-edge',
-                  source: {
-                    cell: hierarchyItem.id,
-                    anchor:
-                      data.type === 'topic-child'
-                        ? {
-                            name: 'right',
-                            args: {
-                              dx: -16,
-                            },
-                          }
-                        : {
-                            name: 'center',
-                            args: {
-                              dx: '25%',
-                            },
-                          },
-                  },
-                  target: {
-                    cell: id,
-                    anchor: {
-                      name: 'left',
-                    },
-                  },
-                })
-              }
-            })
-          }
-        }
-      }
-      traverse(result)
-      graph.centerContent()
-    }
-
-    const findItem = (
-      obj: MindMapData,
-      id: string,
-    ): {
-      parent: MindMapData | null
-      node: MindMapData | null
-    } | null => {
-      if (obj.id === id) {
-        return {
-          parent: null,
-          node: obj,
-        }
-      }
-      const { children } = obj
-      if (children) {
-        for (let i = 0, len = children.length; i < len; i++) {
-          const res = findItem(children[i], id)
-          if (res) {
-            return {
-              parent: res.parent || obj,
-              node: res.node,
-            }
-          }
-        }
-      }
-      return null
-    }
-
-    const addChildNode = (id: string, type: NodeType) => {
-      const res = findItem(data, id)
-      const dataItem = res?.node
-      if (dataItem) {
-        let item: MindMapData | null = null
-        const length = dataItem.children ? dataItem.children.length : 0
-        if (type === 'topic') {
-          item = {
-            id: `${id}-${length + 1}`,
-            type: 'topic-branch',
-            label: `分支主题${length + 1}`,
-            width: 100,
-            height: 40,
-          }
-        } else if (type === 'topic-branch') {
-          item = {
-            id: `${id}-${length + 1}`,
-            type: 'topic-child',
-            label: `子主题${length + 1}`,
-            width: 60,
-            height: 30,
-          }
-        }
-        if (item) {
-          if (dataItem.children) {
-            dataItem.children.push(item)
-          } else {
-            dataItem.children = [item]
-          }
-          return item
-        }
-      }
-      return null
-    }
-
-    const removeNode = (id: string) => {
-      const res = findItem(data, id)
-      const parentItem = res?.parent
-      const nodeItem = res?.node
-      if (parentItem && parentItem.children) {
-        const { children } = parentItem
-        const index = children.findIndex((item) => item.id === id)
-        // 删除的时候，先删节点以及可能存在的子节点，再调用render，对data数据进行遍历
-        if (nodeItem && nodeItem.children) {
-          nodeItem.children.forEach((item) => graph.removeCell(item.id))
-        }
-        graph.removeCell(id)
-        return children.splice(index, 1)
-      }
-      return null
-    }
-
-    graph.on('add:topic', ({ node }: { node: Node }) => {
-      const { id } = node
-      const type = node.prop('type')
-      if (addChildNode(id, type)) {
-        render()
-      }
-    })
-    keyboard.bindKey(['backspace', 'delete'], () => {
-      const selectedNodes = selection
-        .getSelectedCells()
-        .filter((item) => item.isNode())
-      if (selectedNodes.length) {
-        const { id } = selectedNodes[0]
-        if (removeNode(id)) {
-          render()
-        }
-      }
-    })
-
-    keyboard.bindKey('tab', (e) => {
-      e.preventDefault()
-      const selectedNodes = selection
-        .getSelectedCells()
-        .filter((item) => item.isNode())
-      if (selectedNodes.length) {
-        const node = selectedNodes[0]
-        const type = node.prop('type')
-        if (addChildNode(node.id, type)) {
-          render()
-        }
-      }
-    })
-
-    render()
-  }
-
-  refContainer = (container: HTMLDivElement) => {
-    this.container = container
-  }
-
-  render() {
-    return (
-      <div className="x6-graph-wrap mindmap">
-        <div ref={this.refContainer} className="x6-graph" />
-      </div>
-    )
-  }
+    setGraph(graph)
+  }, [setGraph])
+  return (
+    <div className="x6-graph-wrap mindmap">
+      <div ref={container} className="x6-graph" />
+    </div>
+  )
 }
